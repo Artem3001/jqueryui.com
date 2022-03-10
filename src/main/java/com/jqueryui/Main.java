d92109c23b9dce5d3ea927652e5ab7d389a810d1
Artem3001 <art1agarkov@yandex.ru>
2022-03-10 13:51:17 +0500

    Update: driver.manage().window().maximize(); driver.quit();

--- src/main/java/com/jqueryui/Main.java
+++ src/main/java/com/jqueryui/Main.java
@@ -16,6 +16,7 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/webdrivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://jqueryui.com/");
+        driver.manage().window().maximize();
 
         Actions actions = new Actions(driver);
 
@@ -50,6 +51,8 @@ public class Main {
         Assert.assertEquals("Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.", tabText);
         System.out.println(tabText);
         driver.switchTo().defaultContent();
+
+        driver.quit();
     }
 
 }
